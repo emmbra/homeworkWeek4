9c1b2d5ee563de1a33d1f053b4fd7162a6436e57
Emmett Brady <emmett.brady@gmail.com>
2020-03-27 09:19:33 -0700

    added return false to submit button eventlistener

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,6 +106,7 @@ submitButton.addEventListener("click", function(event) {
   event.preventDefault();
   addScore();
   window.location.href = "./highscores.html";
+  return false;
 });
 
 // scoring for answer choices on click event listener
